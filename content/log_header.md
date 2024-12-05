69fc7884edc05eaeb10fe212aad5ded99035ba8c
Matthew Finlayson <mattbnfin@gmail.com>
2024-12-05 14:07:08 -0800

    log

---
+++ content/log_header.md
@@ -0,0 +1,6 @@
+---
+title: Log
+author: '[Matthew Finlayson](index.html)'
+---
+
+Short logs. No organization.
---
+++ render_logs.sh
@@ -0,0 +1,15 @@
+for file in content/log/*
+do
+  pandoc -f markdown --standalone --mathml -t html -o log/$(basename ${file%.md}).html $file
+done
+
+{
+  cat content/log_header.md 
+  for file in content/log/*
+  do
+    echo
+    echo "## $(basename ${file%.md})" # Markdown header with the filename
+    echo
+    cat "$file"
+  done 
+} | pandoc -f markdown --standalone --mathml -t html -o log.html
